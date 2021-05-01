284c071e37434f892698b893cd66381e0221728b
Rishu <rishuanand2552@gmail.com>
2021-05-01 17:39:39 +0530

    1

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req,res) => {image.handleApiCall(req,res)})
 
 
-app.listen(3000, () => {
-    console.log('Master Rian, overall app is running successfully');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Master Rian, overall app is running successfully on port ${process.env.PORT}`);
 });
